faa476155061fb561fcee2ad7de7828e964c57ad
mofirojean <mofirojean@gmail.com>
2025-05-09 17:11:42 +0100

    create template for ai chatapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroPlus, heroArrowPath, heroXMark } from '@ng-icons/heroicons/outline';
+import { heroPaperAirplaneSolid } from '@ng-icons/heroicons/solid';
+import { MarkdownModule } from 'ngx-markdown';
+import { FormsModule } from '@angular/forms';
+
+export interface Chat {
+  from: "user" | "bot",
+  message: string
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    NgIcon,
+    MarkdownModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  viewProviders: [provideIcons({ heroPaperAirplaneSolid, heroPlus, heroArrowPath, heroXMark })]
 })
 export class AppComponent {
-  title = 'gemini-chat-app';
+  constructor() {}
 }
